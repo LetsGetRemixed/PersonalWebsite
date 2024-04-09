aea293e42f96b42f27c695e7ae5f98538e261f43
ColbyPerson <cjp187@txstate.edu>
2024-04-09 03:24:07 -0500

    Home page updates
    Home page update

--- colby-person/src/components/homepage/Home.js
+++ colby-person/src/components/homepage/Home.js
@@ -4,17 +4,26 @@ import Navbar from '../Navigationbar/Navbar';
 
 
 function Home() {
-    return (
-      <div>
-
-
-         <Navbar />
-             <div className="welcome-container">
-                  Welcome
-             </div>
-
+  return (
+    <div>
+      <Navbar />
+      <div className='main-container'>
+          <img src="Self-Pic.jpg" alt="Profile" className="profile-pic"/>
+          <div className="introduction">
+              <div className="name">Colby Person</div>
+              <div className="profession">Software Engineer</div>
+              <div className="ethics">Strong Passion for Coding.</div>
+              <div className="ethics">Strong Work Ethic.</div>
+          </div>
       </div>
-    );
-  }
-  
-  export default Home;
+
+        <div className="glowing-text">
+            Check out my Projects, Resume, Education, and Bio to learn more about me and what I have to offer!
+        </div>
+
+
+    </div>
+  );
+}
+
+export default Home;
