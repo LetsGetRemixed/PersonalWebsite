e45b2ddd09a471e1d747ddcba60741588013b5f9
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2024-04-09 17:41:00 -0500

    socials
    socials

--- colby-person/src/components/homepage/Home.js
+++ colby-person/src/components/homepage/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../homepage/Home.css';
 import Navbar from '../Navigationbar/Navbar';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 function Home() {
@@ -8,12 +9,43 @@ function Home() {
     <div>
       <Navbar />
       <div className='main-container'>
-          <img src="Self-Pic.jpg" alt="Profile" className="profile-pic"/>
-          <div className="introduction">
-              <div className="name">Colby Person</div>
-              <div className="profession">Software Engineer</div>
-              <div className="ethics">Strong Passion for Coding.</div>
-              <div className="ethics">Strong Work Ethic.</div>
+          <div className='intro-container'>
+            <img src="Self-Pic.jpg" alt="Profile" className="profile-pic"/>
+            <div className="introduction">
+               <div className="name">Colby Person</div>
+               <div className="profession">Software Engineer</div>
+               <div className="ethics">Strong Passion for Coding.</div>
+               <div className="ethics">Strong Work Ethic.</div>
+            </div>
+
+            <div className="contact-info">
+                        <div className="contact-item">
+                            <a href="https://github.com/LetsGetRemixed" className="icon-button">
+                                <i className="bi bi-github"></i>
+                            </a>
+                            <a href="https://github.com/LetsGetRemixed" className="contact-lnik" target="_blank" rel="noopener noreferrer">
+                                https://github.com/LetsGetRemixed
+                            </a>
+                        </div>
+                        <div className="contact-item">
+                            <a href="tel:512-786-5133" className="icon-button">
+                                <i className="bi bi-telephone-fill"></i>
+                            </a>
+                            <span className="contact-text">512-786-5133</span>
+                        </div>
+                        <div className="contact-item">
+                            <a href="mailto:colbyperson14@gmail.com" className="icon-button">
+                                <i className="bi bi-envelope-fill"></i>
+                            </a>
+                            <span className="contact-text">colbyperson14@gmail.com</span>
+                        </div> 
+
+
+
+            </div>
+
+
+
           </div>
 
           <div className="glowing-text">
