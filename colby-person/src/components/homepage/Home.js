ac016cc6424ffe66cb2fd57b940a432f48ad0e0f
ColbyPerson <cjp187@txstate.edu>
2024-05-14 03:08:55 -0500

    Home page wraps
    home page wraps for small screens

--- colby-person/src/components/homepage/Home.js
+++ colby-person/src/components/homepage/Home.js
@@ -9,56 +9,43 @@ function Home() {
     <div>
       <Navbar />
       <div className='main-container'>
-          <div className='intro-container'>
-            <img src="Self-Pic.jpg" alt="Profile" className="profile-pic"/>
-            <div className="introduction">
-               <div className="name">Colby Person</div>
-               <div className="profession">Software Engineer</div>
-               <div className="ethics">Strong Passion for Coding.</div>
-               <div className="ethics">Strong Work Ethic.</div>
-            </div>
-
-            <div className="contact-info">
-                        <div className="contact-item">
-                            <a href="https://github.com/LetsGetRemixed" className="icon-button">
-                                <i className="bi bi-github"></i>
-                            </a>
-                            <a href="https://github.com/LetsGetRemixed" className="contact-link" target="_blank" rel="noopener noreferrer">
-                                https://github.com/LetsGetRemixed
-                            </a>
-                        </div>
-                        <div className="contact-item">
-                            <a href="tel:512-786-5133" className="icon-button">
-                                <i className="bi bi-telephone-fill"></i>
-                            </a>
-                            <span className="contact-text">512-786-5133</span>
-                        </div>
-                        <div className="contact-item">
-                            <a href="mailto:colbyperson14@gmail.com" className="icon-button">
-                                <i className="bi bi-envelope-fill"></i>
-                            </a>
-                            <span className="contact-text">colbyperson14@gmail.com</span>
-                        </div> 
-
-
-
-            </div>
-
-
-
+        <div className='intro-container'>
+          <img src="Self-Pic.jpg" alt="Profile" className="profile-pic"/>
+          <div className="introduction">
+            <div className="name">Colby Person</div>
+            <div className="profession">Software Engineer</div>
+            <div className="ethics">Strong Passion for Coding.</div>
+            <div className="ethics">Strong Work Ethic.</div>
           </div>
-
-          <div className="glowing-text">
-            Check out my Projects, Resume, Education, and Bio to learn more about me and what I have to offer!
+          <div className="contact-info">
+            <div className="contact-item">
+              <a href="https://github.com/LetsGetRemixed" className="icon-button">
+                <i className="bi bi-github"></i>
+              </a>
+              <a href="https://github.com/LetsGetRemixed" className="contact-link" target="_blank" rel="noopener noreferrer">
+                https://github.com/LetsGetRemixed
+              </a>
+            </div>
+            <div className="contact-item">
+              <a href="tel:512-786-5133" className="icon-button">
+                <i className="bi bi-telephone-fill"></i>
+              </a>
+              <span className="contact-text">512-786-5133</span>
+            </div>
+            <div className="contact-item">
+              <a href="mailto:colbyperson14@gmail.com" className="icon-button">
+                <i className="bi bi-envelope-fill"></i>
+              </a>
+              <span className="contact-text">colbyperson14@gmail.com</span>
+            </div>
+          </div>
         </div>
-
-          
-
       </div>
-
-        
-
-
+      <div className="footer">
+        <div className="glowing-text">
+          Check out the Projects, Resume, Education, and Bio tabs to learn more about me and what I have to offer!
+        </div>
+      </div>
     </div>
   );
 }
