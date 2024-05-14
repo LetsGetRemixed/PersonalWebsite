fd5e16115ddc43c7a5d48fdacd3205a2b591bbb7
ColbyPerson <cjp187@txstate.edu>
2024-05-14 02:47:53 -0500

    Education page
    full education page

--- colby-person/src/components/Resume/Resume.js
+++ colby-person/src/components/Resume/Resume.js
@@ -11,6 +11,7 @@ function Resume() {
               <div className="resume-header">
                   <h1>My Professional Resume</h1>
                   <p>Below is my resume detailing my skills, experience, and education. You can also <a href="Resumeforwebsite.pdf" download="My_Resume.pdf">download the PDF</a> to view it offline.</p>
+                  <p>All the information in my resume can also be found on this portfolio website.</p>
               </div>
               <div className="resume-container">
                   <object data="Resumeforwebsite.pdf" type="application/pdf" width="100%" height="100%">
